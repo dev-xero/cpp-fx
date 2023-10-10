4857546397fe9919adf0a5c2be27718d878a041b
Xero kun <elo48858@gmail.com>
2023-10-10 14:31:53 +0100

    feat: Added feature to get api key from env variable

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main() {
-    std::cout << "C++ Fx Exchange" << std::endl;
+    const char* api_key = std::getenv("API_KEY");
+
+    if (api_key != nullptr) {
+        std::cout << "API KEY PROVIDED" << std::endl;
+    } else {
+        std::cerr << "API KEY NOT SET" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
